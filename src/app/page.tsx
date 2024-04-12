2bde628d0f9056bb9bb06fefd18061ff4b900af8
Dennis Kamau <34334743+dkamau@users.noreply.github.com>
2024-04-12 11:01:16 +0300

    Add ability to display todo items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,53 @@
-// import tasks from "./tasks";
+"use client";
+
+import tasks from "./tasks";
+import { Task } from "./models/Task";
+import { useState } from "react";
 
 export default function Home() {
+  const [task, setTask] = useState("");
+
+  const addTask = () => {
+    task.push({
+      id: tasks.length + 1,
+      lable: task,
+      checked: false,
+    });
+    setTask("");
+  };
+
   return (
     <main className="flex flex-col items-center justify-center">
       <h1>THINGS TO DO:</h1>
       <hr />
       <p>Looks like you're absolutely free today</p>
       <hr />
-      <p>DONE:</p>
+      <p>DONE: {tasks.filter((t) => t.checked).length}</p>
       <form className="space-x-3">
-        <input placeholder="Enter new task" className="input input-bordered" />
-        <button type="submit" className="btn btn-neutral">Add Task</button>
+        <input
+          placeholder="Enter new task"
+          onChange={(e) => setTask(e.target.value)}
+          className="input input-bordered"
+        />
+        <button type="submit" className="btn btn-neutral" onClick={addTask}>
+          Add Task
+        </button>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <div>
+                <input type="checkbox" defaultChecked={task.checked}></input>
+                {task.checked ? (
+                  <span>
+                    <del>{task.label}</del>
+                  </span>
+                ) : (
+                  <span>{task.label}</span>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
       </form>
     </main>
   );
--- src/app/tasks.ts
+++ src/app/tasks.ts
@@ -1,51 +1,54 @@
-const tasks = [
+import { Task } from "./models/Task";
+
+export const tasks: Task[] = [
   {
     id: 0,
     label: "Display all tasks",
-    checked: false
+    checked: false,
   },
   {
     id: 1,
     label: "Fix layout, checkboxes should be listed in a column",
-    checked: false
+    checked: false,
   },
   {
     id: 2,
     label: "Fix ability to add a new task",
-    checked: false
+    checked: false,
   },
   {
     id: 3,
     label: "Fix ability to toggle a task as checked or unchecked",
-    checked: false
+    checked: false,
   },
   {
     id: 4,
     label: "Fix ability to delete a task",
-    checked: false
+    checked: false,
   },
   {
     id: 5,
     label: "Fix ability to count completed tasks",
-    checked: false
+    checked: false,
   },
   // TIME PERMITTING
   {
     id: 6,
-    label: "Add ability to click on a tasks link and navigate to a page with the task",
-    checked: false
+    label:
+      "Add ability to click on a tasks link and navigate to a page with the task",
+    checked: true,
   },
   {
     id: 7,
     label: "add ability to add notes for a specific task on the tasks page",
-    checked: false
+    checked: false,
   },
   // SUPER TIME PERMITTING
   {
     id: 8,
     label: "save the info in an SQL database",
-    checked: false
-  }
-]
+    checked: false,
+  },
+];
 
-export default tasks
+export default tasks;
